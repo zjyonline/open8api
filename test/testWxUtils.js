3538ca5a4e174c5dc0c26ef19fb9994438fd58ba
jiayong.zhu <jiayong.zhu@qiwo.mobi>
2016-08-20 14:30:21 +0800

    add procfile

--- test/testWxUtils.js
+++ test/testWxUtils.js
@@ -1,5 +1,5 @@
 var wxUtils = require('../utils/wxUtils');
-var url = "http://mp.weixin.qq.com/s?__biz=MjM5MjAxNDM4MA==&mid=2666137041&idx=1&sn=47d1359a3c588610e38afa64b33416dd&scene=0#rd";
+var url = "http://mp.weixin.qq.com/s?__biz=MjI3Njc0NTk4MQ==&mid=2649915715&idx=1&sn=c6228fcd1a32a48928ae9f97a8f8ce48&scene=0#rd";
 wxUtils.getWxArticleData(url,function(err,data){
     console.log(err);
     console.log(data);
