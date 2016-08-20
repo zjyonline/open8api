043ea7a35146756874d9a4f35a1e1cff9fac84e7
jiayong.zhu <jiayong.zhu@qiwo.mobi>
2016-08-20 14:59:28 +0800

    add procfile

--- test/testWxUtils.js
+++ test/testWxUtils.js
@@ -1,5 +1,5 @@
 var wxUtils = require('../utils/wxUtils');
-var url = "http://mp.weixin.qq.com/s?src=3&timestamp=1471673527&ver=1&signature=tu7Z7dFoN0UJrPIBvihajHjA3KplU2q3bbJ-GHCeDYNUH3lZpUW8ltA-riaNdzsLH1fDykC01rH1YZmaXRPXenD-DKPGjIHVZed0wgcNOX4lOBYnWvVNAK4jw7kKlA4aG6y1QrnGIK8CijJQnfYPZQyUsJaMCLOApjIkNGEnWXU=";
+var url = "http://mp.weixin.qq.com/s?src=3&timestamp=1471675327&ver=1&signature=vpRcnDWL4y-O1NScjqfxBhgwSnJGMIjv1sdNvkDUCNa1bPUK*Mw7XsYFtrj55jgl6YMNlP*h*IF5nZnJZW0tCBgYxWHkliyOzuMP9GDgvqSGZCJZmWARwzqPM*IGWOMughRvDHds4Jz6nbdz*3nRKBWSRgz4vqBPIK4JffL9z0M=";
 wxUtils.getWxArticleData(url,function(err,data){
     console.log(err);
     console.log(data);
